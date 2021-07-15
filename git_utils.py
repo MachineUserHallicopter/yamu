71457b7f8eb002d3a369fbbdd6006cd61c599f76
hallicopter <advait@vernacular.ai>
2021-07-16 00:21:53 +0530

    feat: adds method to call shell script for creating repo

--- git_utils.py
+++ git_utils.py
@@ -9,3 +9,9 @@ GITHUB_USER = 'MachineUserHallicopter/'
 def push_file_to_github(filename, content, repo):
     repo = g.get_repo(GITHUB_USER + repo)
     print(repo.create_file(path=filename, content=content, message="adds: autocommit new post", branch="gh-pages"))
+
+
+def update_template(repo):
+    repo = g.get_repo(GITHUB_USER + repo)
+    # Create and push the template
+    os.system('create_repo.sh ' + repo + ' ' + os.getenv('GITHUB_TOKEN'))
