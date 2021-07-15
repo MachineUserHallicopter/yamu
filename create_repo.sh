aa07a47d332535ad7bc6259542b3f5af594f6bfc
hallicopter <advait@vernacular.ai>
2021-07-16 00:22:11 +0530

    feat: script for creating githubpages

---
+++ create_repo.sh
@@ -0,0 +1,9 @@
+gh repo create $1 -y --public
+cd $1/
+cp -r template/* .
+git add .
+git commit -m'init'
+git branch gh-pages
+git checkout gh-pages
+git remote set-url origin https://MachineUserHallicopter:$2@github.com/MachineUserHallicopter/$1.git
+git push origin gh-pages
