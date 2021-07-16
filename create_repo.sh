00b7a08d261ecc33b86643103a8fb23e38fdf569
hallicopter <advait@vernacular.ai>
2021-07-16 19:19:59 +0530

    fix: errors in creation and pushing blog

--- create_repo.sh
+++ create_repo.sh
@@ -1,3 +1,4 @@
+cd ../build
 gh repo create $1 -y --public
 echo 'Created repo'
 git init
@@ -7,7 +8,7 @@ git commit -m'init'
 git branch gh-pages
 git checkout gh-pages
 echo 'Setting remote'
-git remote remove origin
+#git remote remove origin
 git remote add origin https://MachineUserHallicopter:$2@github.com/MachineUserHallicopter/$1.git
-git push -u origin gh-page
+git push -u origin gh-pages
 echo 'Pushed and done'
