2ee187641f5ccd72a7a1b5385e9fe7a0ca773e53
hallicopter <advait@vernacular.ai>
2021-07-16 11:24:26 +0530

    init

--- git_utils.py
+++ git_utils.py
@@ -1,6 +1,6 @@
 import os
-
 from github import Github
+import yaml
 
 g = Github(os.getenv('GITHUB_TOKEN'))
 GITHUB_USER = 'MachineUserHallicopter/'
@@ -12,6 +12,13 @@ def push_file_to_github(filename, content, repo):
 
 
 def update_template(repo):
-    repo = g.get_repo(GITHUB_USER + repo)
+    file_name = 'template/_config.yml'
+    print("title: "+repo+"\nbaseurl: '/"+repo+"/' # name of the repository")
+    file = open(file_name, "a")  # append mode
+    file.write("title: "+repo+"\nbaseurl: '/"+repo+"/' # name of the repository")
+    file.close()
+
     # Create and push the template
-    os.system('create_repo.sh ' + repo + ' ' + os.getenv('GITHUB_TOKEN'))
+    os.system('./create_repo.sh ' + repo + ' ' + os.getenv('GITHUB_TOKEN'))
+
+update_template("yamu")
